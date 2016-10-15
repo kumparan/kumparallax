7c67aad4d2c692be5e056c19c078a5f22d7fcb43
agung_sb <agungsuryabangsa@kumparan.com>
2016-10-15 20:41:48 +0700

    Improve the demo

--- src/App.js
+++ src/App.js
@@ -15,9 +15,26 @@ class App extends Component {
     return (
       <Kumparallax
         parallaxImage="https://media.creativemornings.com/uploads/user/avatar/216237/Rachel-Profile-Square-06.05.14.jpg"
-        parallaxHeader={<div style={{ height: 400 }}>This is the parallax header</div>}
+        parallaxHeader={
+          <div style={{ padding: 10 }}>
+            <span style={{ fontSize: 70, color: '#FFF' }}>This is the parallax header!</span>
+            <br/>
+            <span style={{ fontSize: 20, color: 'aqua' }}>By default, the parallax height is 400, but you can put an element here.</span>
+            <br/>
+            <span style={{ fontSize: 50, color: 'cyan' }}>If there is an element, the parallax height will suit the height of the element.</span>
+            <br/>
+            <span style={{ fontSize: 50, color: 'brown' }}>Yowezlah...</span>
+          </div>}
         parallaxContent={
           <div>
+            <div className="box"><h2>Content</h2></div>
+            <div className="box"><h2>in</h2></div>
+            <div className="box"><h2>boxes</h2></div>
+            <div className="box"><h2>to</h2></div>
+            <div className="box"><h2>make</h2></div>
+            <div className="box"><h2>the</h2></div>
+            <div className="box"><h2>page</h2></div>
+            <div className="box"><h2>scroll</h2></div>
             <div className="box"><h2>Content</h2></div>
             <div className="box"><h2>in</h2></div>
             <div className="box"><h2>boxes</h2></div>
