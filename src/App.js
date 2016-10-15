112ab3d8a8bd5e71f5b7742635cec4f34ecf0714
agung_sb <agungsuryabangsa@kumparan.com>
2016-10-16 06:12:30 +0700

    Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import { render } from "react-dom";
 import Kumparallax from './Kumparallax';
 
 class App extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
-    let inc = () => {
-      let oldcount = this.state.count;
-      this.setState({ count: oldCount + 1 });
-    }
     return (
       <Kumparallax
         parallaxImage="https://media.creativemornings.com/uploads/user/avatar/216237/Rachel-Profile-Square-06.05.14.jpg"
         parallaxHeader={
           <div style={{ padding: 10 }}>
             <span style={{ fontSize: 70, color: '#FFF' }}>This is the parallax header!</span>
-            <br/>
+            <br />
             <span style={{ fontSize: 20, color: 'aqua' }}>By default, the parallax height is 400, but you can put an element here.</span>
-            <br/>
+            <br />
             <span style={{ fontSize: 50, color: 'cyan' }}>If there is an element, the parallax height will suit the height of the element.</span>
-            <br/>
+            <br />
             <span style={{ fontSize: 50, color: 'brown' }}>Yowezlah...</span>
           </div>}
         parallaxContent={
